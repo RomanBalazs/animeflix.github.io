6a2b8ac321e8dcf3e5bd9b6caf36db9d68ce5503
xDragon989 <32239278+RomanBalazs@users.noreply.github.com>
2026-02-18 15:53:44 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,2 +1,23 @@
-window.ANIMEFLIX_DATA = window.ANIMEFLIX_DATA || { legalContent: {} };
+/* global window */
+/**
+ * Jogtiszta videók forrásai IDE kerülnek (YouTube embed / saját MP4 stb.)
+ * A katalógus (AniList) mindenkinek reklámmentes.
+ */
+window.ANIMEFLIX_DATA = window.ANIMEFLIX_DATA || {
+  legalContent: {
+    /*
+      Példa:
+      "15125": {
+        noteHu: "YouTube jogtiszta forrás.",
+        seasons: [
+          { season: 1, episodes: [
+            { id: "ep1", title: "1. rész", type: "youtube", src: "https://www.youtube.com/embed/..." }
+          ]}
+        ]
+      }
+    */
+  }
+};
+
+// extra biztosíték: globál név is legyen (régi kódok miatt)
 var ANIMEFLIX_DATA = window.ANIMEFLIX_DATA;
