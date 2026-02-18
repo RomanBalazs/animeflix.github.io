2fec7833e9378a4de0b48200441889bfc2f73db1
xDragon989 <32239278+RomanBalazs@users.noreply.github.com>
2026-02-18 10:35:57 +0100

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// base: './' -> GitHub Pages aldomain + repo alútvonalon stabil (relatív assetek)
-export default defineConfig({
-  plugins: [react()],
-  base: "./"
-});
